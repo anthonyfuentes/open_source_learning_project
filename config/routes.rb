1c8f31b58643bf78d5d917d63af2dc0f36ebf00c
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-21 21:58:22 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root "resources#new"
+  root "resources#index"
+
+  resources :users, only: [:show]
   resources :resources, only: [:new, :create, :show, :index] do
     resources :feedbacks, only: [:new, :create]
   end
