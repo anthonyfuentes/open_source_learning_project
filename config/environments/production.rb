fae6bc1ac13889b68a4ca78ed102d52ffbfd7dbb
John Sawyer <sawyer@sawyermerchant.com>
2016-12-22 09:49:05 -0500

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "open_source_learning_project_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+   :address        => 'smtp.sendgrid.net',
+   :port           => '587',
+   :authentication => :plain,
+   :user_name      => ENV['SENDGRID_USERNAME'],
+   :password       => ENV['SENDGRID_PASSWORD'],
+   :domain         => 'heroku.com',
+   :enable_starttls_auto => true
+ }
+ config.action_mailer.delivery_method ||= :smtp
+
+ config.action_mailer.default_url_options = {
+   :host => 'https://peaceful-waters-19907.herokuapp.com',
+ }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
