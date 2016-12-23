3241b8589a3cbdd22e91079a7b6a814420b243f4
John Sawyer <sawyer@sawyermerchant.com>
2016-12-23 08:33:05 -0500

    Additional small change to average time method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,13 @@ module ApplicationHelper
     return "Time to Complete\nNot Yet Rated" if feedbackable.feedbacks.empty?
 
     if completion_time < 60
-      "~#{completion_time} mins"
+      "~#{completion_time} mins\nAverage Completion Time"
     else
       completion_time /= 60
-      "~#{completion_time} hrs"
+      "~#{completion_time} hrs\nAverage Completion Time"
     end
   end
-  
+
   def display_tags(taggable) # TODO: is taggable the right name here?
     tag_names = taggable.tags.map { |tag| tag.name }
     tag_names.join(', ')
