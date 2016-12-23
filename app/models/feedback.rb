3353c839218502f831316bcf7984cfab2535e406
Jessica G <gillan.jessica@gmail.com>
2016-12-23 12:19:44 -0700

    add unique validation on feedbacks

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,9 @@
 class Feedback < ApplicationRecord
   belongs_to :user
   belongs_to :feedbackable, polymorphic: true
+
   validates :rating, presence: true
   validates :difficulty, presence: true
+
+  validates :user_id, uniqueness: { scope: [:feedbackable_type, :feedbackable_id] }
 end
