58e1e4b97142c331ace2f504f8419d77b943f458
John Sawyer <sawyer@sawyermerchant.com>
2016-12-23 15:23:46 -0500

    Change config/environments/development for sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = {
+    :host => 'https://peaceful-waters-19907.herokuapp.com',
   }
 
   config.action_mailer.perform_caching = false
