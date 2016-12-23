23dbce3a6bc9afc05283367110f4d73715d81f01
Pramod Jacob <psjacob68@gmail.com>
2016-12-22 21:25:27 -0500

    working on rendering comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
 
   def create
     @commentable = extract_commentable
-    fail
+    @comment = @commentable.comments.build(comment_params)
+    @comment.save ? successful_create : failed_create
   end
 
   private
@@ -10,6 +11,19 @@ class CommentsController < ApplicationController
     def extract_commentable
       resource, id = request.path.split('/')[1,2]
       resource.singularize.classify.constantize.find(id)
+    end
+
+    def successful_create
+      redirect_back fallback_location: root_path
+    end
+
+    def failed_create
+      flash[:error] = "Sorry, we couldn't create a comment." # TODO: remove?
+      redirect_back fallback_location: root_path
+    end
+
+    def comment_params
+      params.require(:comment).permit(:body)
     end
 
 end
